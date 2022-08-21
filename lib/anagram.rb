b5e488b7e0c3d94ecbe6925df8e40b1358ba6e3b
+ <+>
2022-08-21 15:01:30 +0300

    commit tree ae29548309e4c390783f1e00e4115cfb0dc477db

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+     def initialize(word)
+        @word=word
+     end
+def match(possible_matches)
+   possible_matches.filter do |p_match| p_match.split("").sort==@word.split("").sort
+   end
+end
+end
+diaper=Anagram.new("diaper")
+pp diaper.match(%w[hello world zombies pants diaper])
+
